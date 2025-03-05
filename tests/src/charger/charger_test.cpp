8afe6eaf457d5c438472d5c575428753d17cd472
Kyunghwan Kwon <k@pazzk.net>
2025-03-05 23:31:24 +0900

    fix test cases

--- tests/src/charger/charger_test.cpp
+++ tests/src/charger/charger_test.cpp
@@ -52,103 +52,103 @@ TEST(Charger, ShouldReturnNull_WhenNoConnectorAttached) {
 	LONGS_EQUAL(NULL, charger_get_connector(&charger, NULL));
 }
 TEST(Charger, ShouldAttachConnector) {
-	struct connector *c = (struct connector *)0x1234;
+	struct connector c;
 	int id;
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c));
 	LONGS_EQUAL(1, charger_count_connectors(&charger));
-	LONGS_EQUAL(0, charger_get_connector_id(&charger, c, &id));
+	LONGS_EQUAL(0, charger_get_connector_id(&charger, &c, &id));
 	LONGS_EQUAL(1, id);
 }
 TEST(Charger, ShouldReturnConnector_WhenConnectorIdGiven) {
-	struct connector *c = (struct connector *)0x1234;
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c));
-	LONGS_EQUAL(c, charger_get_connector_by_id(&charger, 1));
+	struct connector c;
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c));
+	LONGS_EQUAL(&c, charger_get_connector_by_id(&charger, 1));
 }
 TEST(Charger, ShouldReturnAvailableConnector_WhenAvailable) {
-	struct connector *c = (struct connector *)0x1234;
+	struct connector c;
 	mock().expectOneCall("connector_is_enabled").andReturnValue(true);
 	mock().expectOneCall("connector_is_reserved").andReturnValue(false);
 	mock().expectOneCall("connector_state").andReturnValue(A);
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c));
-	LONGS_EQUAL(c, charger_get_connector_available(&charger));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c));
+	LONGS_EQUAL(&c, charger_get_connector_available(&charger));
 }
 TEST(Charger, ShouldDetachConnector) {
-	struct connector *c = (struct connector *)0x1234;
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c));
+	struct connector c;
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c));
 	LONGS_EQUAL(1, charger_count_connectors(&charger));
-	LONGS_EQUAL(0, charger_detach_connector(&charger, c));
+	LONGS_EQUAL(0, charger_detach_connector(&charger, &c));
 	LONGS_EQUAL(0, charger_count_connectors(&charger));
 }
 TEST(Charger, ShouldReturnCountOfConnectors_WhenConnectorAttached) {
-	struct connector *c1 = (struct connector *)1;
-	struct connector *c2 = (struct connector *)2;
-	struct connector *c3 = (struct connector *)3;
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c1));
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c2));
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c3));
+	struct connector c1;
+	struct connector c2;
+	struct connector c3;
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c1));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c2));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c3));
 	LONGS_EQUAL(3, charger_count_connectors(&charger));
 }
 TEST(Charger, ShouldReturnUniqueConnectorID_WhenMultipleConnectorsAttached) {
-	struct connector *c1 = (struct connector *)1;
-	struct connector *c2 = (struct connector *)2;
-	struct connector *c3 = (struct connector *)3;
+	struct connector c1;
+	struct connector c2;
+	struct connector c3;
 	int id1, id2, id3;
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c1));
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c2));
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c3));
-	LONGS_EQUAL(0, charger_get_connector_id(&charger, c1, &id1));
-	LONGS_EQUAL(0, charger_get_connector_id(&charger, c2, &id2));
-	LONGS_EQUAL(0, charger_get_connector_id(&charger, c3, &id3));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c1));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c2));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c3));
+	LONGS_EQUAL(0, charger_get_connector_id(&charger, &c1, &id1));
+	LONGS_EQUAL(0, charger_get_connector_id(&charger, &c2, &id2));
+	LONGS_EQUAL(0, charger_get_connector_id(&charger, &c3, &id3));
 	LONGS_EQUAL(1, id1);
 	LONGS_EQUAL(2, id2);
 	LONGS_EQUAL(3, id3);
 }
 TEST(Charger, ShouldReturnUniqueConnectorID_WhenMultipleConnectorsAttachedAndDetached) {
-	struct connector *c1 = (struct connector *)1;
-	struct connector *c2 = (struct connector *)2;
-	struct connector *c3 = (struct connector *)3;
+	struct connector c1;
+	struct connector c2;
+	struct connector c3;
 	int id1, id2, id3;
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c1));
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c2));
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c3));
-	LONGS_EQUAL(0, charger_get_connector_id(&charger, c1, &id1));
-	LONGS_EQUAL(0, charger_get_connector_id(&charger, c2, &id2));
-	LONGS_EQUAL(0, charger_get_connector_id(&charger, c3, &id3));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c1));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c2));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c3));
+	LONGS_EQUAL(0, charger_get_connector_id(&charger, &c1, &id1));
+	LONGS_EQUAL(0, charger_get_connector_id(&charger, &c2, &id2));
+	LONGS_EQUAL(0, charger_get_connector_id(&charger, &c3, &id3));
 	LONGS_EQUAL(1, id1);
 	LONGS_EQUAL(2, id2);
 	LONGS_EQUAL(3, id3);
-	LONGS_EQUAL(0, charger_detach_connector(&charger, c2));
+	LONGS_EQUAL(0, charger_detach_connector(&charger, &c2));
 	LONGS_EQUAL(2, charger_count_connectors(&charger));
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c2));
-	LONGS_EQUAL(0, charger_get_connector_id(&charger, c2, &id2));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c2));
+	LONGS_EQUAL(0, charger_get_connector_id(&charger, &c2, &id2));
 	LONGS_EQUAL(4, id2);
 	LONGS_EQUAL(3, charger_count_connectors(&charger));
 }
 TEST(Charger, ShouldProcessAllConnectors) {
-	struct connector *c1 = (struct connector *)1;
-	struct connector *c2 = (struct connector *)2;
-	struct connector *c3 = (struct connector *)3;
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c1));
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c2));
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c3));
+	struct connector c1;
+	struct connector c2;
+	struct connector c3;
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c1));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c2));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c3));
 
-	mock().expectOneCall("connector_process").withParameter("self", c1);
-	mock().expectOneCall("connector_process").withParameter("self", c2);
-	mock().expectOneCall("connector_process").withParameter("self", c3);
+	mock().expectOneCall("connector_process").withParameter("self", &c1);
+	mock().expectOneCall("connector_process").withParameter("self", &c2);
+	mock().expectOneCall("connector_process").withParameter("self", &c3);
 
 	LONGS_EQUAL(0, charger_process(&charger));
 }
 TEST(Charger, ShouldReturnConnectorID_WhenConnectorGiven) {
-	struct connector *c1 = (struct connector *)1;
-	struct connector *c2 = (struct connector *)2;
-	struct connector *c3 = (struct connector *)3;
+	struct connector c1;
+	struct connector c2;
+	struct connector c3;
 	int id1, id2, id3;
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c1));
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c2));
-	LONGS_EQUAL(0, charger_attach_connector(&charger, c3));
-	LONGS_EQUAL(0, charger_get_connector_id(&charger, c1, &id1));
-	LONGS_EQUAL(0, charger_get_connector_id(&charger, c2, &id2));
-	LONGS_EQUAL(0, charger_get_connector_id(&charger, c3, &id3));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c1));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c2));
+	LONGS_EQUAL(0, charger_attach_connector(&charger, &c3));
+	LONGS_EQUAL(0, charger_get_connector_id(&charger, &c1, &id1));
+	LONGS_EQUAL(0, charger_get_connector_id(&charger, &c2, &id2));
+	LONGS_EQUAL(0, charger_get_connector_id(&charger, &c3, &id3));
 	LONGS_EQUAL(1, id1);
 	LONGS_EQUAL(2, id2);
 	LONGS_EQUAL(3, id3);
